bae472ff7b0d58b18b0838451f5f8153c515a08f
arvin <arvingrep@gmail.com>
2023-12-20 15:45:12 +0800

    update jenkins

--- EKS/manage/jenkins.tf
+++ EKS/manage/jenkins.tf
@@ -1,3 +1,11 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+}
+
 resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://charts.jenkins.io"
@@ -8,10 +16,11 @@ resource "helm_release" "jenkins" {
   timeout    = 600
   values = [
     file("values.yaml"),
-  ]
-
-  depends_on = [
-    kubernetes_namespace.jenkins,
+    <<EOF
+jenkinsaws:
+  awsaccesskey: ${var.aws_access_key_id}
+  awssecretkey: ${var.aws_secret_access_key}
+EOF
   ]
 }
 
